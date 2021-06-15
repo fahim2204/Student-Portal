6757dcc741c2a3462508ef665eabb04e327c088d
Fahim Faisal <fahimfaisal1998@gmail.com>
2021-06-15 10:12:47 +0600

    Started Project

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\LoginController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// Route::get('/login', function () {
+//     return view('login.index');
+// });
+// Route::get('/login', ['uses' => 'LoginController@index']);
+Route::get('/login', [LoginController::class,'index']);
+Route::post('/login', [LoginController::class,'Verify']);
+Route::get('/registration', function () {
+    return view('registration.index');
+});
+
+//Route::get('/home', ['as'=>'home.index ','uses'=>'HomeController@index']);
