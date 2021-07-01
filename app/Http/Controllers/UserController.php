f5ff0e7b24324757248b33f342a2ec35203042a8
ssalvi8586 <ssalvi8586@gmail.com>
2021-07-01 19:47:28 +0600

    update all

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,6 +13,7 @@ use App\Models\Instructor;
 use App\Models\Admin;
 use App\Models\Qualification;
 use Illuminate\Support\Carbon;
+use Validator;
 
 
 class UserController extends Controller
@@ -55,7 +56,7 @@ class UserController extends Controller
             ->with('slinks', $slinks);
     }
 
-    public function update(editRequest $req)
+    public function update(Request $req)
     {
 
         // $user = User::where('uname', $req->session()->get('uname'))
@@ -68,8 +69,16 @@ class UserController extends Controller
 
         // $type = $user->type;
 
-
+    if($req->has('form1')){
         $type = $req->session()->get('type');
+        $req->validate([
+            'name'      => 'required|min:3',
+            'email'     => 'required',
+            'contact'   => 'required|regex:/(01)[0-9]{9}/',
+            'address'   => 'required'
+            ]);
+
+        
         //dd($type);
         if ($type == 'moderator') {
             moderator::where('fr_user_id', $req->session()->get('id'))
@@ -100,20 +109,27 @@ class UserController extends Controller
         } elseif ($type == 'student') {
             student::where('fr_user_id', $req->session()->get('id'))
                 ->update([
-                    'name' => $req->name,
-                    'email' => $req->email,
-                    'address' => $req->address,
+                    'name'       => $req->name,
+                    'email'      => $req->email,
+                    'address'    => $req->address,
                     'updated_at' => Carbon::now(),
-                    'contact' => $req->contact
+                    'contact'    => $req->contact
                 ]);
 
             $req->session()->flash('msg', 'Update Successful');
             return redirect()->route('profile.edit');
             //return view('profile.edit');
         }
+    }
 
-        elseif($req->newpass!=null){
-            if($req->newpasse===$req->confirmpass){
+    elseif($req->has('form2')){
+        $req->validate([
+            'oldpass'       => 'required',
+            'newpass'       => 'required|min:6|regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%]).*$/',
+            'confirmpass'   => 'required|same:newpass'
+            ]);
+       if($req->newpass!=null){
+            if($req->newpass===$req->confirmpass){
                     $user = User::where('uname', $req->session()->get('uname'))
 
 
@@ -121,14 +137,14 @@ class UserController extends Controller
                 $password = $user->password;
 
                 if ($req->oldpass == $password) {
-                    user::where('fr_user_id', $req->session()->get('id'))
+                    User::where('uname', $req->session()->get('uname'))
                         ->update([
                             'password' => $req->newpass
                         ]);
                     $req->session()->flash('msg', 'Update Successful');
                     return redirect()->route('profile.edit');
                 } else {
-                    $req->session()->flash('error', 'Unauthorized Access');
+                    $req->session()->flash('error', 'Give Old Password');
                     return redirect()->route('profile.edit');
                 }
             } else {
@@ -139,8 +155,47 @@ class UserController extends Controller
         }
         else{
 
-            $req->session()->flash('error', 'Check Again');
+            $req->session()->flash('error', 'Fill All the Fields');
             return redirect()->route('profile.edit');
         }
     }
+
+
+
+
+    elseif($req->has('delete')){
+
+        
+        $user = User::where('uname', $req->session()->get('uname'))
+                    ->first();
+                $password = $user->password;
+
+                $req->validate([
+                    
+                    'confirmpass'   => 'required|same:$password'
+                    ]);
+                if ($req->confirmpass == $password) {
+                    // User::where('uname', $req->session()->get('uname'))
+                    //     ->update([
+                    //         'password' => $req->newpass
+                    //     ]);
+                    }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+    }
+
+
+
+    
 }
